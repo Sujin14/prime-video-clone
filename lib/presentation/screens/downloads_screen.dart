aa454c74bbe2a76d276a302adcb556be08ea9761
Sujin14 <sujinsuresh1422002@gmail.com>
2025-04-23 17:17:55 +0530

    Initial screen setup with Home, Store, Search, Downloads navigation

---
+++ lib/presentation/screens/downloads_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DownloadsScreen extends StatelessWidget {
+  const DownloadsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Downloads',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
